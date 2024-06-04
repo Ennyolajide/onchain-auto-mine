ea6364305c9a7979ff87f5a2a2f0ddf29d3b489c
Olajide Eniseyin <eniseyinolajide@gmail.com>
2024-06-05 00:35:45 +0100

    fix Too much request issue

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const { chainClick, chainRefill, logInfo, logInfoError, logError, exitProcess } 
 
 const authData = buildAuthQuery();
 
-axios.post(urls.validate, authData, { headers: getHeaders(authData) })
-    .then((res) => {
-        const { success, token } = res.data;
-        (success && token) ? false : process.exit();
-        const auth = token ? setToken(token) : false;
-        setTimeout(() => {
+async function main() {
+    axios.post(urls.validate, authData, { headers: getHeaders(authData) })
+        .then((res) => {
+            const { success, token } = res.data;
+            (success && token) ? false : process.exit();
+            const auth = token ? setToken(token) : false;
             axios.get(urls.info, { headers: getHeaders({}, auth) }).then((res) => {
                 const { success, user } = res.data;
                 success ? logInfo(user) : logInfoError();
                 const { energy, clickLevel, dailyEnergyRefill } = user;
                 setTimeout(() => {
                     (dailyEnergyRefill && energy <= 100) ? chainRefill(auth) : false;
-                },3000);
+                }, 3000);
 
                 function handleChainclick() {
                     (energy >= 0) ? chainClick(auth, clicks(energy, clickLevel)) : exitProcess();
@@ -28,15 +28,16 @@ axios.post(urls.validate, authData, { headers: getHeaders(authData) })
                 handleChainclick();
 
                 setInterval(handleChainclick, (15 * 1000));
-                
+
             }).catch((error) => {
                 logError(error);
                 process.exit()
             });
-        }, 3000);
+        })
+        .catch(error => {
+            logError(error);
+            process.exit();
+        });
+}
 
-    })
-    .catch(error => {
-        logError(error);
-        process.exit();
-    });
+main();
